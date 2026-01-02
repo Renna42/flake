426e8af965bc0ffd2cd381edb91ce9d90c5ee9e0
Renna Z. <is@renna.dev>
2026-01-02 20:36:22 +0800

    i18n

--- modules/home/base.nix
+++ modules/home/base.nix
@@ -8,7 +8,7 @@
     homeDirectory = "/home/${username}";
     sessionVariables = {
       LANG = "zh_CN.UTF-8";
-      LANGUAGE = "zh_CN:en_US";
+      LANGUAGE = "zh_CN:zh:ja_JP:ja:en_US:en";
     };
     stateVersion = "25.11";
   };
--- modules/nixos/i18n.nix
+++ modules/nixos/i18n.nix
@@ -4,19 +4,21 @@ _: {
   i18n = {
     defaultLocale = "en_US.UTF-8";
     supportedLocales = [
+      "C.UTF-8/UTF-8"
       "zh_CN.UTF-8/UTF-8"
       "en_US.UTF-8/UTF-8"
+      "ja_JP.UTF-8/UTF-8"
     ];
     extraLocaleSettings = {
-      LC_ADDRESS = "zh_CN.UTF-8";
-      LC_IDENTIFICATION = "zh_CN.UTF-8";
-      LC_MEASUREMENT = "zh_CN.UTF-8";
-      LC_MONETARY = "zh_CN.UTF-8";
-      LC_NAME = "zh_CN.UTF-8";
-      LC_NUMERIC = "zh_CN.UTF-8";
-      LC_PAPER = "zh_CN.UTF-8";
-      LC_TELEPHONE = "zh_CN.UTF-8";
-      LC_TIME = "zh_CN.UTF-8";
+      LC_ADDRESS = "C.UTF-8";
+      LC_IDENTIFICATION = "C.UTF-8";
+      LC_MEASUREMENT = "C.UTF-8";
+      LC_MONETARY = "C.UTF-8";
+      LC_NAME = "C.UTF-8";
+      LC_NUMERIC = "C.UTF-8";
+      LC_PAPER = "C.UTF-8";
+      LC_TELEPHONE = "C.UTF-8";
+      LC_TIME = "C.UTF-8";
     };
   };
 }
