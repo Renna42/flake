a1430bbfa4ad9142cb0ec1e6569fd9c75f229243
Renna Z. <is@renna.dev>
2026-01-03 17:32:15 +0800

    using hyprland

--- hosts/Mizuka.nix
+++ hosts/Mizuka.nix
@@ -7,7 +7,7 @@
   imports = [
     ../modules/nixos
     ../modules/roles/desktop
-    ../modules/desktop/greetd.nix
+    ../modules/desktop/suites/hyprland.nix
     ../modules/desktop/suites/plasma.nix
     ../modules/hardware/nvidia.nix
     ../modules/hardware/intel-gfx.nix
