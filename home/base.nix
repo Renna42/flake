e0b62658b0765b6e094d81f51c9ff3ad19a9d1d8
Renna Z. <is@renna.dev>
2026-01-11 21:01:53 +0800

    home.stateVersion = lib.mkDefault

--- home/base.nix
+++ home/base.nix
@@ -16,7 +16,7 @@ in {
       LANG = "en_US.UTF-8";
       LANGUAGE = "en_US:en";
     };
-    stateVersion = "25.11";
+    stateVersion = lib.mkDefault "25.11";
   };
   xdg.userDirs = lib.mkIf (system != "aarch64-darwin") {
     enable = true;
