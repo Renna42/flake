0e0790dc82a59b72b185531dc7bddf2f6ced7fd4
Renna Z. <is@renna.dev>
2026-01-18 01:07:26 +0800

    set authorized-keys

--- nixos/users/renna.nix
+++ nixos/users/renna.nix
@@ -19,6 +19,9 @@ in {
     ];
     shell = pkgs.fish;
     initialHashedPassword = "$y$j9T$KHYs8lBhE5S.gupM7N/QE/$zurxi/XMT5n6aACZu9tz3RBLBQ6Ge/eCUwODOjRMqe0";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHL5pMTK8LGrizHB2VvgL1RG9cNKxAhYXb59NqSyAwpw"
+    ];
   };
   programs.fish = {
     enable = true;
