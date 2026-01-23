3bdb41ac6e7d811a7f0205431d5dcea6d58a4442
Renna Z. <is@renna.dev>
2026-01-23 12:17:25 +0800

    wakatime: use `lib.generators.toINI`

--- home/apps/wakatime/default.nix
+++ home/apps/wakatime/default.nix
@@ -1,14 +1,12 @@
 {
   config,
-  pkgs,
+  lib,
   ...
-}: let
-  format = pkgs.formats.ini {};
-in {
+}: {
   sops.secrets.wakatime_api_key = {};
 
   home.file.".wakatime.cfg" = {
-    source = format.generate ".wakatime.cfg" {
+    text = lib.generators.toINI {} {
       settings.api_key_vault_cmd = "cat ${config.sops.secrets.wakatime_api_key.path}";
     };
     force = true;
