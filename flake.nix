ea86d1f349c57f4a571b50be187b6b08cbbc0def
Renna Z. <is@renna.dev>
2026-01-23 09:41:09 +0800

    Workaround for sops-nix on Darwin

--- flake.nix
+++ flake.nix
@@ -26,7 +26,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
-      url = "github:Mic92/sops-nix";
+      # Workaround for sops-nix on Darwin
+      # https://github.com/Mic92/sops-nix/issues/890
+      url = "github:Mic92/sops-nix?rev=d7593b87b0c1c33f9cfdd485a7fef081dd5362e7";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     git-hooks.url = "github:cachix/git-hooks.nix";
