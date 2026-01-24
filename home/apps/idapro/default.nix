541ed0bf93b06a491f1864cc19cbe41a20ff7a40
Renna Z. <is@renna.dev>
2026-01-24 23:39:24 +0800

    add catppuccin theme for ida

--- home/apps/idapro/default.nix
+++ home/apps/idapro/default.nix
@@ -1,3 +1,21 @@
-{assetsPath, ...}: {
-  home.file.".idapro/idapro.hexlic".source = "${assetsPath}/idapro.hexlic";
+{
+  assetsPath,
+  config,
+  pkgs,
+  ...
+}: {
+  home.file = {
+    ".idapro/idapro.hexlic".source = "${assetsPath}/idapro.hexlic";
+    ".idapro/themes/catppuccin-${config.catppuccin.flavor}" = {
+      source =
+        pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "ida-debugger";
+          rev = "22f43b265c03f4b77fe6bcd9b09dcc3705e3092d";
+          hash = "sha256-aFTEgZ8eWlOOqoQsiQ5d/eqbkMGSjb5ImZPlFhlh/2g=";
+        }
+        + "/catppuccin-${config.catppuccin.flavor}";
+      recursive = true;
+    };
+  };
 }
