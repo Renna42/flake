6a16c2a6185e8f2e2d66118d5f6a429a5d1229f9
Renna Z. <is@renna.dev>
2026-01-24 15:33:46 +0800

    add lutris

--- home/apps/game.nix
+++ home/apps/game.nix
@@ -3,4 +3,21 @@
     prismlauncher
     osu-lazer-bin
   ];
+
+  programs.lutris = {
+    enable = true;
+    defaultWinePackage = pkgs.proton-ge-bin;
+    protonPackages = [
+      pkgs.proton-ge-bin
+    ];
+    extraPackages = with pkgs; [
+      mangohud
+      winetricks
+      gamescope
+      gamemode
+      mesa-demos # for glxinfo
+    ];
+  };
+  # Lutris runtime has some incompatible libraries
+  home.sessionVariables.LUTRIS_RUNTIME = "0";
 }
--- nixos/hardware/nvidia.nix
+++ nixos/hardware/nvidia.nix
@@ -5,6 +5,7 @@
 }: {
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
     extraPackages = with pkgs; [
       nvidia-vaapi-driver
     ];
@@ -14,7 +15,6 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = true;
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
