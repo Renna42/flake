3f67b24a18c9baa89bc5530c3a6efafc55400454
Renna Z. <is@renna.dev>
2026-01-29 18:05:17 +0800

    openscreen: sync with my upstream

--- packages/openscreen/package.nix
+++ packages/openscreen/package.nix
@@ -9,112 +9,111 @@
   electron_39,
   nodejs_22,
   nix-update-script,
-}: let
-  electron = electron_39;
-in
-  buildNpmPackage (finalAttrs: {
-    pname = "openscreen";
-    version = "1.1.0";
-    nodejs = nodejs_22;
+  nodejs ? nodejs_22,
+  electron ? electron_39,
+}:
+buildNpmPackage (finalAttrs: {
+  inherit nodejs;
 
-    src = fetchFromGitHub {
-      owner = "siddharthvaddem";
-      repo = "openscreen";
-      tag = "v${finalAttrs.version}";
-      hash = "sha256-eneZ/2oCjIimlqGpwKGdxiPPsDTdaBOHpTUfWOQSD54=";
-    };
+  pname = "openscreen";
+  version = "1.1.0";
 
-    npmDepsHash = "sha256-TNUbF0YVpAGZFGYf3cgM2oTrSGTo6tTvckqLrvdiE6g=";
+  src = fetchFromGitHub {
+    owner = "siddharthvaddem";
+    repo = "openscreen";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-eneZ/2oCjIimlqGpwKGdxiPPsDTdaBOHpTUfWOQSD54=";
+  };
 
-    nativeBuildInputs = [
-      makeWrapper
-      copyDesktopItems
-    ];
+  npmDepsHash = "sha256-TNUbF0YVpAGZFGYf3cgM2oTrSGTo6tTvckqLrvdiE6g=";
 
-    env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+  nativeBuildInputs = [
+    makeWrapper
+    copyDesktopItems
+  ];
 
-    patches = [
-      ./electron-avoid-phantomjs.patch
-    ];
+  env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
 
-    buildPhase = ''
-      runHook preBuild
+  patches = [
+    # Avoid downloading `phantomjs` within the build sandbox
+    # by completely removing `electron-icon-builder`
+    ./electron-avoid-phantomjs.patch
+  ];
 
-      npm exec tsc
-      npm exec vite build
+  buildPhase = ''
+    runHook preBuild
 
-      ${lib.optionalString stdenv.hostPlatform.isDarwin ''
-        # electronDist needs to be modifiable on Darwin
-        cp -r ${electron.dist} electron-dist
-        chmod -R u+w electron-dist
+    npm exec tsc
+    npm exec vite build
 
-        # Disable code signing during build on macOS.
-        # https://github.com/electron-userland/electron-builder/blob/fa6fc16/docs/code-signing.md#how-to-disable-code-signing-during-the-build-process-on-macos
-        export CSC_IDENTITY_AUTO_DISCOVERY=false
-        npm exec electron-builder -- \
-          --dir \
-          -c.electronDist=electron-dist \
-          -c.electronVersion=${electron.version}
-      ''}
+    ${lib.optionalString stdenv.hostPlatform.isDarwin ''
+      # electronDist needs to be modifiable on Darwin
+      cp -r ${electron.dist} electron-dist
+      chmod -R u+w electron-dist
+      # Disable code signing during build on macOS.
+      # https://github.com/electron-userland/electron-builder/blob/fa6fc16/docs/code-signing.md#how-to-disable-code-signing-during-the-build-process-on-macos
+      export CSC_IDENTITY_AUTO_DISCOVERY=false
+    ''}
+    ${lib.optionalString stdenv.hostPlatform.isLinux ''
+      ln ${electron.dist} electron-dist
+    ''}
 
-      ${lib.optionalString stdenv.hostPlatform.isLinux ''
-        npm exec electron-builder -- \
-          --dir \
-          -c.electronDist=${electron.dist} \
-          -c.electronVersion=${electron.version}
-      ''}
+    npm exec electron-builder -- \
+      --dir \
+      -c.electronDist=electron-dist \
+      -c.electronVersion=${electron.version}
 
-      runHook postBuild
-    '';
+    runHook postBuild
+  '';
 
-    installPhase = ''
-      runHook preInstall
+  installPhase = ''
+    runHook preInstall
 
-      ${lib.optionalString stdenv.hostPlatform.isLinux ''
-        mkdir -p $out/share/openscreen
-        cp -r release/*/*-unpacked/{locales,resources{,.pak}} $out/share/openscreen
+    ${lib.optionalString stdenv.hostPlatform.isLinux ''
+      mkdir -p $out/share/openscreen
+      cp -r release/*/*-unpacked/{locales,resources{,.pak}} $out/share/openscreen
 
-        makeWrapper ${lib.getExe electron} $out/bin/openscreen \
-            --add-flags $out/share/openscreen/resources/app.asar \
-            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
-            --set-default ELECTRON_IS_DEV 0 \
-            --inherit-argv0
+      makeWrapper ${lib.getExe electron} $out/bin/openscreen \
+          --add-flags $out/share/openscreen/resources/app.asar \
+          --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
+          --set-default ELECTRON_IS_DEV 0 \
+          --inherit-argv0
 
-        install -Dm644 icons/icons/png/512x512.png $out/share/icons/hicolor/512x512/apps/openscreen.png
-      ''}
+      install -Dm644 icons/icons/png/512x512.png $out/share/icons/hicolor/512x512/apps/openscreen.png
+    ''}
 
-      ${lib.optionalString stdenv.hostPlatform.isDarwin ''
-        mkdir -p $out/Applications
-        cp -r release/*/mac*/"Openscreen.app" $out/Applications/
-        makeWrapper "$out/Applications/Openscreen.app/Contents/MacOS/Openscreen" $out/bin/openscreen
-      ''}
+    ${lib.optionalString stdenv.hostPlatform.isDarwin ''
+      mkdir -p $out/Applications
+      cp -R release/*/mac*/Openscreen.app $out/Applications/
+      makeWrapper $out/Applications/Openscreen.app/Contents/MacOS/Openscreen $out/bin/openscreen
+    ''}
 
-      runHook postInstall
-    '';
+    runHook postInstall
+  '';
 
-    desktopItems = [
-      (makeDesktopItem {
-        name = "openscreen";
-        desktopName = "OpenScreen";
-        comment = finalAttrs.meta.description;
-        icon = "openscreen";
-        exec = "openscreen %u";
-        categories = [
-          "AudioVideo"
-          "Video"
-          "Utility"
-        ];
-        terminal = false;
-      })
-    ];
+  desktopItems = [
+    (makeDesktopItem {
+      name = "openscreen";
+      desktopName = "OpenScreen";
+      comment = finalAttrs.meta.description;
+      icon = "openscreen";
+      exec = "openscreen %u";
+      categories = [
+        "AudioVideo"
+        "Video"
+        "Utility"
+      ];
+      terminal = false;
+    })
+  ];
 
-    passthru.updateScript = nix-update-script {};
+  passthru.updateScript = nix-update-script {};
 
-    meta = {
-      inherit (electron.meta) platforms;
-      description = "OpenScreen is your free, open-source alternative to Screen Studio (sort of).";
-      homepage = "https://openscreen.vercel.app";
-      license = lib.licenses.mit;
-      mainProgram = "openscreen";
-    };
-  })
+  meta = {
+    description = "Free, open-source alternative to Screen Studio (sort of)";
+    homepage = "https://openscreen.vercel.app";
+    license = lib.licenses.mit;
+    mainProgram = "openscreen";
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+})
