297a7a6232e3ad01c74bf9de7ab60ca01cda2c16
Renna Z. <is@renna.dev>
2026-02-06 05:24:44 +0800

    nix: trusted-public-keys -> extra-trusted-public-keys

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -51,7 +51,7 @@
           "https://cache.garnix.io"
           "https://renna42.cachix.org"
         ];
-        trusted-public-keys = [
+        extra-trusted-public-keys = [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
           "renna42.cachix.org-1:AqHSiL2lFKYHYJ0U2YFiW1kjItvFMmyyc6loFZR3/X8="
