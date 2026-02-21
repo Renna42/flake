289b649c816c665a1b4224e8a9d967c559a5e081
Renna Z. <is@renna.dev>
2026-02-21 23:45:49 +0800

    dwproton: idk what I did

--- pkgs/dwproton-bin.nix
+++ pkgs/dwproton-bin.nix
@@ -2,16 +2,14 @@
   fetchzip,
   proton-ge-bin,
 }:
-proton-ge-bin.overrideAttrs (
-  finalAttrs: _previousAttrs: {
-    pname = "dwproton-bin";
-    version = "10.0-18";
+proton-ge-bin.overrideAttrs (finalAttrs: {
+  pname = "dwproton-bin";
+  version = "10.0-18";
 
-    steamDisplayName = "dwproton";
+  steamDisplayName = "dwproton";
 
-    src = fetchzip {
-      url = "https://dawn.wine/dawn-winery/dwproton/releases/download/dwproton-${finalAttrs.version}/dwproton-${finalAttrs.version}-x86_64.tar.xz";
-      hash = "sha256-v87DiRf/NFMeDa0D9Td24zIZOvU5fIZ5JfNfLSAYGXc=";
-    };
-  }
-)
+  src = fetchzip {
+    url = "https://dawn.wine/dawn-winery/dwproton/releases/download/dwproton-${finalAttrs.version}/dwproton-${finalAttrs.version}-x86_64.tar.xz";
+    hash = "sha256-v87DiRf/NFMeDa0D9Td24zIZOvU5fIZ5JfNfLSAYGXc=";
+  };
+})
