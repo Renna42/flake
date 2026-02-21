105a912519cc9ac6dbb3886f05ac515050c3e387
Renna Z. <is@renna.dev>
2026-02-21 23:46:27 +0800

    use vpl-gpu-rt for intel-gfx

--- nixos/hardware/intel-gfx.nix
+++ nixos/hardware/intel-gfx.nix
@@ -4,9 +4,12 @@
     enable32Bit = true;
     extraPackages = with pkgs; [
       intel-media-driver
-      intel-vaapi-driver
-      libva-vdpau-driver
-      libvdpau-va-gl
+      vpl-gpu-rt
+      intel-compute-runtime
     ];
   };
+
+  services.xserver.videoDrivers = ["modesetting"];
+
+  environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
 }
