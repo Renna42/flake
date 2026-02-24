30bc2c6ab477ca57280f22260089148b13a71cb0
Renna Z. <is@renna.dev>
2026-02-24 10:30:31 +0800

    flake: remove a unused comment

--- flake.nix
+++ flake.nix
@@ -56,7 +56,6 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # Using kernel 6.18 version because NVIDIA breaked it.
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
     git-hooks-nix.url = "github:cachix/git-hooks.nix";
     treefmt-nix.url = "github:numtide/treefmt-nix";
