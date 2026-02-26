d5da87ffcd3042f454a2a2be6588432b1b914ef2
Renna Z. <is@renna.dev>
2026-02-27 00:22:25 +0800

    kitty: format the code

--- home/apps/kitty/default.nix
+++ home/apps/kitty/default.nix
@@ -14,7 +14,7 @@
       enableFishIntegration = true;
       # make kitty dont alias sudo to make doas works well
       # ref: https://github.com/NixOS/nixpkgs/issues/260427#issuecomment-1758197272
-      mode = lib.optionalString (! pkgs.stdenv.isDarwin) "no-sudo";
+      mode = lib.optionalString (!pkgs.stdenv.isDarwin) "no-sudo";
     };
 
     keybindings = {
