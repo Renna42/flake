35323e9af9b6d8c3983c9650a0bb05b02140d5e2
Renna Z. <is@renna.dev>
2026-03-23 22:43:38 +0800

    kmscon: use font config from stylix

--- nixos/services/kmscon.nix
+++ nixos/services/kmscon.nix
@@ -1,16 +1,8 @@
-{pkgs, ...}: {
+_: {
   services.kmscon = {
     enable = true;
-    fonts = [
-      {
-        name = "Maple Mono Normal NF CN";
-        package = pkgs.maple-mono.Normal-NF-CN-unhinted;
-      }
-      {
-        name = "Unifont";
-        package = pkgs.unifont;
-      }
-    ];
-    hwRender = true;
+    extraConfig = ''
+      font-dpi=120
+    '';
   };
 }
