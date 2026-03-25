0df560a6bd165ca671ea7278cc8c9fa9d66280f0
Renna Z. <is@renna.dev>
2026-03-25 11:14:53 +0800

    darwin: disable tailscale service

--- darwin/darwin-system.nix
+++ darwin/darwin-system.nix
@@ -147,8 +147,6 @@
     fish
   ];
 
-  services.tailscale.enable = true;
-
   environment.variables.SOPS_AGE_KEY_FILE = "$HOME/.config/sops/age/keys.txt";
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-darwin";
