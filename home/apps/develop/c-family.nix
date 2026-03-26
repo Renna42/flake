f3535445a48ff23d5eca790de89d6eb4f6626459
Renna Z. <is@renna.dev>
2026-03-26 21:35:27 +0800

    darwin: already have some depends at Xcode

--- home/apps/develop/c-family.nix
+++ home/apps/develop/c-family.nix
@@ -1,15 +1,23 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    # keep-sorted start
-    clang-analyzer
-    clang-tools
-    cmake
-    gcc
-    gdb
-    gnumake
-    lldb
-    meson
-    ninja
-    # keep-sorted end
-  ];
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs;
+    [
+      # keep-sorted start
+      clang-analyzer
+      cmake
+      gcc
+      gdb
+      gnumake
+      meson
+      ninja
+      # keep-sorted end
+    ]
+    ++ (lib.optionals pkgs.stdenv.isLinux [
+      # Darwin platform have LLVM toolchain from Xcode
+      clang-tools
+      lldb
+    ]);
 }
