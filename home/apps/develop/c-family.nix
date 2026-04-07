6e32782313015b082f04f465c6880715848be35f
Renna Z. <is@renna.dev>
2026-04-07 20:42:38 +0800

    c-family: add some tools

--- home/apps/develop/c-family.nix
+++ home/apps/develop/c-family.nix
@@ -6,17 +6,21 @@
   home.packages = with pkgs;
     [
       # keep-sorted start
+      automake
       clang-analyzer
       cmake
-      gcc
+      gdb
       gnumake
       meson
+      nasm
       ninja
+      pkg-config
       # keep-sorted end
     ]
     ++ (lib.optionals pkgs.stdenv.isLinux [
       # Darwin platform have LLVM toolchain from Xcode
       clang-tools
+      gcc
       lldb
     ]);
 }
