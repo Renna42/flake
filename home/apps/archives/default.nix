0147ff911f5ea00a3cfceea7a3bd1ae722b892fd
Renna Z. <is@renna.dev>
2026-04-17 22:53:50 +0800

    archives: add wimlib & brotli & cpio

--- home/apps/archives/default.nix
+++ home/apps/archives/default.nix
@@ -2,13 +2,16 @@
   home.packages = with pkgs; [
     # keep-sorted start
     _7zip-zstd-rar
+    brotli
     bzip2_1_1
+    cpio
     gnutar
     gzip
     lz4
     rar
     unrar
     unzipNLS
+    wimlib
     xz
     zip
     zstd
