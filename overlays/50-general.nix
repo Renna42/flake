50ed0da38807fdb1a35e04cca3b0034d9cd31988
Renna Z. <is@renna.dev>
2026-05-19 01:59:31 +0800

    overlays: drop the override of aria2

--- overlays/50-general.nix
+++ overlays/50-general.nix
@@ -1,36 +1,5 @@
 _: final: prev: {
   # keep-sorted start block=yes
-  aria2 = prev.aria2.overrideAttrs (
-    new: old: {
-      version = "1.37.0-11";
-      src = final.fetchFromGitHub {
-        owner = "AnInsomniacy";
-        repo = "aria2-builder";
-        rev = "v${new.version}";
-        sha256 = "sha256-xbiNSg/Z+CA0x0DQfMNsWdA+TATyX6dCeW2Nf3L3Kfs=";
-      };
-
-      buildInputs =
-        old.buildInputs
-        ++ [
-          final.openssl
-          final.expat
-        ];
-      configureFlags =
-        old.configureFlags
-        ++ [
-          "--without-gnutls"
-          "--with-openssl"
-          "--without-libxml2"
-          "--with-libexpat"
-          "--with-libcares"
-          "--with-libz"
-          "--with-sqlite3"
-          "--with-libssh2"
-          "--disable-nls"
-        ];
-    }
-  );
   mpv-unwrapped = prev.mpv-unwrapped.override {
     inherit (final.nur-xddxdd.lantianCustomized) ffmpeg;
   };
