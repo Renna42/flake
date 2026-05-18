939f6237523cd4a9f281b5650469145c137d35f0
Renna Z. <is@renna.dev>
2026-05-19 01:59:19 +0800

    overlays: drop the patch of wireshark

--- overlays/30-patches.nix
+++ overlays/30-patches.nix
@@ -1,15 +1,4 @@
-_: final: prev:
-{
-  wireshark = prev.wireshark.overrideAttrs (new: old: {
-    src = prev.fetchFromGitLab {
-      repo = "wireshark";
-      owner = "wireshark";
-      tag = "v${new.version}";
-      hash = "sha256-Zvrwxjp4LK2J3QnxmPxKKrU01YHQvPyp54UWzeGNCjA=";
-    };
-  });
-}
-// prev.lib.mkIf prev.stdenv.isLinux {
+_: final: prev: {
   openldap = prev.openldap.overrideAttrs {
     doCheck = false;
   };
