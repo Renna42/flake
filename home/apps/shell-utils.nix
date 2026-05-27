67371bf012d07a0cd889b11ad91442a08cf97439
Renna Z. <is@renna.dev>
2026-05-27 14:16:12 +0800

    shell-utils: programs.ssh.matchBlocks -> programs.ssh.settings

--- home/apps/shell-utils.nix
+++ home/apps/shell-utils.nix
@@ -99,7 +99,7 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = {
+    settings = {
       "*" = {
         forwardAgent = false;
         addKeysToAgent = "no";
