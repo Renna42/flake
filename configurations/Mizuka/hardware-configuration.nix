0175d5c9ac7d2161d26154ee1a4599e1e2fbbbd1
Renna Z. <is@renna.dev>
2026-07-02 16:32:16 +0800

    Mizuka: drop the configuration for DAC

--- configurations/Mizuka/hardware-configuration.nix
+++ configurations/Mizuka/hardware-configuration.nix
@@ -33,18 +33,6 @@
   # To make sops-nix happy
   fileSystems."/home".neededForBoot = true;
 
-  services.pipewire.wireplumber.extraConfig."fosi-audio-zh3" = {
-    "monitor.alsa.rules" = [
-      {
-        matches = [{"node.name" = "alsa_output.usb-Fosi_Fosi_Audio_ZH3-00.analog-stereo";}];
-        actions.update-props = {
-          "audio.format" = "S32LE";
-          "audio.rate" = 768000;
-        };
-      }
-    ];
-  };
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
