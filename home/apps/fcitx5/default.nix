a9598e1706beb3a7daee24fba143be6da8043070
Renna Z. <is@renna.dev>
2026-07-03 04:14:49 +0800

    home: refactoring fcitx5 configuration with rime

--- home/apps/fcitx5/default.nix
+++ home/apps/fcitx5/default.nix
@@ -1,3 +1,35 @@
-{lib, ...}: {
-  xdg.configFile."fcitx5".source = lib.mkForce ./configs;
+{pkgs, ...}: let
+  fcitx5-rime-with-addons =
+    (pkgs.fcitx5-rime.override {
+      librime = pkgs.nur-xddxdd.lantianCustomized.librime-with-plugins;
+      rimeDataPkgs = with pkgs; [
+        renna.rime-data
+        rime-data
+      ];
+    }).overrideAttrs
+    (old: {
+      # Prebuild schema data
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.parallel];
+      postInstall =
+        (old.postInstall or "")
+        + ''
+          for F in $out/share/rime-data/*.schema.yaml; do
+            echo "rime_deployer --compile "$F" $out/share/rime-data $out/share/rime-data $out/share/rime-data/build" >> parallel.lst
+          done
+          parallel -j$(nproc) < parallel.lst || true
+        '';
+    });
+in {
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-rime-with-addons
+        fcitx5-mozc-ut
+        kdePackages.fcitx5-qt
+      ];
+    };
+  };
 }
--- home/linux-desktop.nix
+++ home/linux-desktop.nix
@@ -111,20 +111,5 @@ in {
     };
   };
 
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5 = {
-      waylandFrontend = true;
-      addons = with pkgs; [
-        qt6Packages.fcitx5-chinese-addons
-        qt6Packages.fcitx5-configtool
-        fcitx5-pinyin-zhwiki
-        # fcitx5-mozc
-        fcitx5-fluent
-      ];
-    };
-  };
-
   fonts.fontconfig.enable = true;
 }
