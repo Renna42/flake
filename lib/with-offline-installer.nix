243020454f561725d60f942b5e038f3f8dbeae41
Renna Z. <is@renna.dev>
2026-07-03 05:59:28 +0800

    garnix: drop it

--- lib/with-offline-installer.nix
+++ lib/with-offline-installer.nix
@@ -57,11 +57,9 @@ in
                   "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
                   "https://mirrors.ustc.edu.cn/nix-channels/store"
                   "https://nix-community.cachix.org"
-                  "https://cache.garnix.io"
                 ];
                 trusted-public-keys = [
                   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                  "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
                 ];
               };
 
