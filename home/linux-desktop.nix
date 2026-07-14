8097a77eea4befaba95cbb661f3cf1246f6e4a99
Renna Z. <is@renna.dev>
2026-07-14 19:13:29 +0800

    linux-desktop: Firefox has enabled WebRender defaultly since version 84

--- home/linux-desktop.nix
+++ home/linux-desktop.nix
@@ -70,7 +70,6 @@ in {
     sessionVariables = {
       "NIXOS_OZONE_WL" = "1"; # for any ozone-based browser & electron apps to run on wayland
       "MOZ_ENABLE_WAYLAND" = "1"; # for firefox to run on wayland
-      "MOZ_WEBRENDER" = "1";
       # enable native Wayland support for most Electron apps
       "ELECTRON_OZONE_PLATFORM_HINT" = "auto";
 
