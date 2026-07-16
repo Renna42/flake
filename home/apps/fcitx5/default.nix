6d306f3f0e31062c2991895067229002741b3f63
Renna Z. <is@renna.dev>
2026-07-16 14:48:38 +0800

    fcitx5: add settings

--- home/apps/fcitx5/default.nix
+++ home/apps/fcitx5/default.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
@@ -6,11 +10,44 @@
       waylandFrontend = true;
       addons = with pkgs; [
         fcitx5-mozc-ut
-        fcitx5-pinyin-zhwiki
-        fcitx5-pinyin-moegirl
-        kdePackages.fcitx5-chinese-addons
         kdePackages.fcitx5-qt
       ];
+      settings = {
+        globalOptions = {
+          Hotkey = {
+            EnumerateWithTriggerKeys = true;
+            EnumerateSkipFirst = false;
+          };
+          Behavior = {
+            ActiveByDefault = true;
+            resetStateWhenFocusIn = "No";
+            ShareInputState = "All";
+            showInputMethodInformationWhenFocusIn = true;
+          };
+        };
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "keyboard-us";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "rime";
+          "Groups/0/Items/2".Name = "mozc";
+        };
+        addons = {
+          classicui = {
+            globalSection = {
+              Font = lib.mkForce "MiSans 12";
+              UseInputMethodLanguageToDisplayText = true;
+              PerScreenDPI = false;
+              ForceWaylandDPI = 0;
+              EnableFractionalScale = true;
+            };
+          };
+        };
+      };
     };
   };
 }
