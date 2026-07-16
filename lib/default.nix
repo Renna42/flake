56dc23b72cc6b9a2fc324bc928aaa4d68419c5aa
Renna Z. <is@renna.dev>
2026-07-16 14:51:24 +0800

    lib: drop withOfflineInstaller

--- lib/default.nix
+++ lib/default.nix
@@ -1,14 +1,3 @@
 {lib}: {
   makePackages = import ./make-packages.nix {inherit lib;};
-  withOfflineInstaller = {
-    flake,
-    nixosConfig,
-  }:
-    import ./with-offline-installer.nix {
-      inherit
-        flake
-        nixosConfig
-        lib
-        ;
-    };
 }
