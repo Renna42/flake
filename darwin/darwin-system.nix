96e4da25c679ccc4f57b0062607d860dc3b32cf2
Renna Z. <is@renna.dev>
2026-07-20 19:07:21 +0800

    darwin-system: sudo_local.reattach

--- darwin/darwin-system.nix
+++ darwin/darwin-system.nix
@@ -130,6 +130,7 @@
     pam.services.sudo_local = {
       touchIdAuth = true;
       watchIdAuth = true;
+      reattach = true;
     };
     pki.certificateFiles = [
       "${pkgs.dn42-cacert}/etc/ssl/certs/dn42-ca.crt"
