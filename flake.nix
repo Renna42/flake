194fc6a51f761051321b673863808126971886a1
Renna Z. <is@renna.dev>
2026-07-28 04:21:35 +0800

    flake: drop renna42.cachix.org

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
       "https://linyinfeng.cachix.org"
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
-      "https://renna42.cachix.org"
       # keep-sorted end
     ];
     extra-trusted-public-keys = [
@@ -21,7 +20,6 @@
       "linyinfeng.cachix.org-1:sPYQXcNrnCf7Vr7T0YmjXz5dMZ7aOKG3EqLja0xr9MM="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "renna42.cachix.org-1:AqHSiL2lFKYHYJ0U2YFiW1kjItvFMmyyc6loFZR3/X8="
       # keep-sorted end
     ];
   };
