e9503adfcb877aa99a509650c15985e051e38319
Renna Z. <is@renna.dev>
2026-08-31 19:56:05 +0800

    overlays: do not override mpv-unwarppered

--- overlays/50-general.nix
+++ overlays/50-general.nix
@@ -1,7 +1,2 @@
 _: final: prev: {
-  # keep-sorted start block=yes
-  mpv-unwrapped = prev.mpv-unwrapped.override {
-    inherit (final.nur-xddxdd.lantianCustomized) ffmpeg;
-  };
-  # keep-sorted end
 }
