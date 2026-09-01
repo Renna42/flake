621e58e9825cab61cea78bed27cecd72f616416d
Renna Z. <is@renna.dev>
2026-09-02 03:03:02 +0800

    NixOS: simply applying two patches

--- overlays/30-patches.nix
+++ overlays/30-patches.nix
@@ -1,1 +1,13 @@
-_: final: prev: {}
+_: final: prev: {
+  ddcci-driver = prev.ddcci-driver.overrideAttrs (oldAttrs: {
+    patches =
+      [
+        (prev.fetchpatch {
+          name = "Use-sysfs_emit-and-field-width-specifier.patch";
+          url = "https://gitlab.com/liquidnya/ddcci-driver-linux/-/commit/9510aa4aebf32678884f55ae251e54012a354ed1.patch";
+          hash = "sha256-s12ers7nPFaHOB+8/S8t3dtdoR6slukkfNPdghgftNs=";
+        })
+      ]
+      ++ (oldAttrs.patches or []);
+  });
+}
