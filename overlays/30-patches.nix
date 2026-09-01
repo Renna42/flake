a4ba95def0e9b734cd2b2de4c4bf08058502f07d
Renna Z. <is@renna.dev>
2026-09-02 03:19:37 +0800

    nixos: try to fix ddcci

--- overlays/30-patches.nix
+++ overlays/30-patches.nix
@@ -1,13 +1,11 @@
 _: final: prev: {
-  ddcci-driver = prev.ddcci-driver.overrideAttrs (oldAttrs: {
-    patches =
-      [
-        (prev.fetchpatch {
-          name = "Use-sysfs_emit-and-field-width-specifier.patch";
-          url = "https://gitlab.com/liquidnya/ddcci-driver-linux/-/commit/9510aa4aebf32678884f55ae251e54012a354ed1.patch";
-          hash = "sha256-s12ers7nPFaHOB+8/S8t3dtdoR6slukkfNPdghgftNs=";
-        })
-      ]
-      ++ (oldAttrs.patches or []);
+  ddcci-driver = prev.ddcci-driver.overrideAttrs (new: old: {
+    src = prev.fetchFromGitLab {
+      # https://gitlab.com/dkadioglu/ddcci-driver-linux/-/tree/replace-strncpy-strscpy
+      owner = "dkadioglu";
+      repo = "ddcci-driver-linux";
+      rev = "db5d6b87b2c85ff91a4470c50a4da99534d9917c";
+      hash = "sha256-l8e9J2ZVtH5rJ47toi+A1K0mneQu1W/ik2cQHz7QRwY=";
+    };
   });
 }
