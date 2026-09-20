08c8267f14bd5b33a813a843040fa50277d9c3c5
Renna Z. <is@renna.dev>
2026-09-21 00:57:08 +0800

    ayugram-irena: it may works

--- pkgs/ayugram-irena.nix
+++ pkgs/ayugram-irena.nix
@@ -2,9 +2,12 @@
   ayugram-desktop,
   sources,
 }:
-ayugram-desktop.overrideAttrs (new: old: {
-  inherit (sources.ayugram-irena) src;
-
+ayugram-desktop.overrideAttrs {
   pname = "ayugram-irena";
-  version = "7.1.2-unstable-${sources.ayugram-irena.date or sources.ayugram-irena.verison}";
-})
+  unwrapped = ayugram-desktop.unwrapped.overrideAttrs (new_: old_: {
+    inherit (sources.ayugram-irena) src;
+
+    pname = "ayugram-irena-unwrapped";
+    version = "7.1.2-unstable-${sources.ayugram-irena.date or sources.ayugram-irena.verison}";
+  });
+}
