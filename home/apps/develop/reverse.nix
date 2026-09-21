02efc8b1f4fad72a43dcfd846e8a028264365fde
Renna Z. <is@renna.dev>
2026-09-21 16:48:42 +0800

    reverse: add die

--- home/apps/develop/reverse.nix
+++ home/apps/develop/reverse.nix
@@ -1,11 +1,20 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  unstablePkgs,
+  ...
+}: {
   imports = [
     ../idapro
   ];
 
-  home.packages = with pkgs; [
-    (binwalk.override {
-      enableUnfree = true;
-    })
-  ];
+  home.packages = with unstablePkgs;
+    [
+      (binwalk.override {
+        enableUnfree = true;
+      })
+    ]
+    ++ (lib.optionals (!pkgs.stdenv.isDarwin) [
+      detect-it-easy
+    ]);
 }
