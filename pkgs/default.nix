bfaa4bbe260a3b39ee0fbd910e92b9fc81d6b5cf
Renna Z. <is@renna.dev>
2026-09-23 05:19:46 +0800

    exactaudiocopy: wine requires x86 platform

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,26 +6,29 @@
 lib.makeScope newScope (
   self: let
     inherit (self) callPackage;
-  in {
-    # keep-sorted start block=yes
-    aegisub-arch1t3cht = callPackage ./aegisub-arch1t3cht.nix {};
-    catppuccin-obsidian = callPackage ./catppuccin-obsidian.nix {};
-    exactaudiocopy = callPackage ./exactaudiocopy.nix {};
-    fsdecrypt = callPackage ./fsdecrypt.nix {};
-    gen-rdk = callPackage ./gen-rdk {};
-    harletty-bridge = callPackage ./harletty-bridge.nix {};
-    ilass = callPackage ./ilass.nix {};
-    lapse = callPackage ./lapse.nix {};
-    lapse-silero = callPackage ./lapse.nix {
-      withSilero = true;
-    };
-    libfvad = callPackage ./libfvad.nix {};
-    linuxPackages_xanmod_latest = {
-      smifb2 = pkgs.linuxPackages_xanmod_latest.callPackage ./smifb2 {};
-    };
-    obsidian-livesync = callPackage ./obsidian-livesync.nix {};
-    obsidian-tasks = callPackage ./obsidian-tasks.nix {};
-    rime-data = callPackage ./rime-data {};
-    # keep-sorted end
-  }
+  in
+    {
+      # keep-sorted start block=yes
+      aegisub-arch1t3cht = callPackage ./aegisub-arch1t3cht.nix {};
+      catppuccin-obsidian = callPackage ./catppuccin-obsidian.nix {};
+      fsdecrypt = callPackage ./fsdecrypt.nix {};
+      gen-rdk = callPackage ./gen-rdk {};
+      harletty-bridge = callPackage ./harletty-bridge.nix {};
+      ilass = callPackage ./ilass.nix {};
+      lapse = callPackage ./lapse.nix {};
+      lapse-silero = callPackage ./lapse.nix {
+        withSilero = true;
+      };
+      libfvad = callPackage ./libfvad.nix {};
+      linuxPackages_xanmod_latest = {
+        smifb2 = pkgs.linuxPackages_xanmod_latest.callPackage ./smifb2 {};
+      };
+      obsidian-livesync = callPackage ./obsidian-livesync.nix {};
+      obsidian-tasks = callPackage ./obsidian-tasks.nix {};
+      rime-data = callPackage ./rime-data {};
+      # keep-sorted end
+    }
+    // pkgs.hostPlatform.isx86 {
+      exactaudiocopy = callPackage ./exactaudiocopy.nix {};
+    }
 )
